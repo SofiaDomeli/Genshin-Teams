229711084cbb985e318f97836b009ddbbfa90e63
Sofia Domeli <softeliware@gmail.com>
2024-11-17 16:18:16 -0300

    tudo feito

--- app.py
+++ app.py
@@ -1,52 +1,113 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
 
 app = Flask(__name__)
 app.secret_key = 'sua_chave_secreta'  # Necessário para exibir mensagens flash
 
-# Lista de personagens de Genshin Impact
-personagens = [
-    'Aether', 'Lumine', 'Amber', 'Barbara', 'Beidou', 'Bennett', 'Chongyun', 'Diluc',
-    'Fischl', 'Ganyu', 'Hu Tao', 'Jean', 'Kazuha', 'Keqing', 'Klee', 'Mona', 'Ningguang',
-    'Qiqi', 'Razor', 'Rosaria', 'Sucrose', 'Tartaglia', 'Venti', 'Xiangling', 'Xiao',
-    'Xin Yan', 'Yae Miko', 'Zhongli'
-]
-
-# Função para gerar a URL da imagem
-def gerar_url_imagem(nome_personagem):
-    nome_personagem_modificado = nome_personagem.replace(" ", "_").lower()
-    return f"https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/{nome_personagem_modificado}/image.png?strip=all&quality=100&w=160"
+characters = {
+    'Albedo': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/albedo/image.png?strip=all&quality=100&w=160',
+    'Alhaitham': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/alhaitham/image.png?strip=all&quality=100&w=160',
+    'Amber': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/amber/image.png?strip=all&quality=100&w=160',
+    'Itto': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/arataki_itto/image.png?strip=all&quality=100&w=160',
+    'Arlecchino': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/arlecchino/image.png?strip=all&quality=100&w=160',
+    'Baizhu': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/baizhu/image.png?strip=all&quality=100&w=160',
+    'Barbara': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/barbara/image.png?strip=all&quality=100&w=160',
+    'Beidou': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/beidou/image.png?strip=all&quality=100&w=160',
+    'Bennett': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/bennett/image.png?strip=all&quality=100&w=160',
+    'Candace': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/candace/image.png?strip=all&quality=100&w=160',
+    'Charlotte': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/charlotte/image.png?strip=all&quality=100&w=160',
+    'Chevreuse': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/chevreuse/image.png?strip=all&quality=100&w=160',
+    'Chiori': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/chiori/image.png?strip=all&quality=100&w=160',
+    'Chongyun': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/chongyun/image.png?strip=all&quality=100&w=160',
+    'Clorinde': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/clorinde/image.png?strip=all&quality=100&w=160',
+    'Collei': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/collei/image.png?strip=all&quality=100&w=160',
+    'Cyno': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/cyno/image.png?strip=all&quality=100&w=160',
+    'Dehya': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/dehya/image.png?strip=all&quality=100&w=160',
+    'Diluc': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/diluc/image.png?strip=all&quality=100&w=160',
+    'Diona': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/diona/image.png?strip=all&quality=100&w=160',
+    'Dori': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/dori/image.png?strip=all&quality=100&w=160',
+    'Emilie': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/emilie/image.png?strip=all&quality=100&w=160',
+    'Eula': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/eula/image.png?strip=all&quality=100&w=160',
+    'Faruzan': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/faruzan/image.png?strip=all&quality=100&w=160',
+    'Fischl': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/fischl/image.png?strip=all&quality=100&w=160',
+    'Freminet': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/freminet/image.png?strip=all&quality=100&w=160',
+    'Furina': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/furina/image.png?strip=all&quality=100&w=160',
+    'Gaming': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/gaming/image.png?strip=all&quality=100&w=160',
+    'Ganyu': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/ganyu/image.png?strip=all&quality=100&w=160',
+    'Gorou': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/gorou/image.png?strip=all&quality=100&w=160',
+    'Hu Tao': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/hu_tao/image.png?strip=all&quality=100&w=160',
+    'Jean': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/jean/image.png?strip=all&quality=100&w=160',
+    'Kachina': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kachina/image.png?strip=all&quality=100&w=160',
+    'Kazuha': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kaedehara_kazuha/image.png?strip=all&quality=100&w=160',
+    'Kaeya': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kaeya/image.png?strip=all&quality=100&w=160',
+    'Ayaka': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kamisato_ayaka/image.png?strip=all&quality=100&w=160',
+    'Ayato': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kamisato_ayato/image.png?strip=all&quality=100&w=160',
+    'Kaveh': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kaveh/image.png?strip=all&quality=100&w=160',
+    'Keqing': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/keqing/image.png?strip=all&quality=100&w=160',
+    'Kinich': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kinich/image.png?strip=all&quality=100&w=160',
+    'Kirara': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kirara/image.png?strip=all&quality=100&w=160',
+    'Klee': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/klee/image.png?strip=all&quality=100&w=160',
+    'Sara': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kujou_sara/image.png?strip=all&quality=100&w=160',
+    'Shinobu': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kuki_shinobu/image.png?strip=all&quality=100&w=160',
+    'Layla': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/layla/image.png?strip=all&quality=100&w=160',
+    'Lisa': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/lisa/image.png?strip=all&quality=100&w=160',
+    'Lyney': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/lyney/image.png?strip=all&quality=100&w=160',
+    'Lynette': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/lynette/image.png?strip=all&quality=100&w=160',
+    'Mika': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/mika/image.png?strip=all&quality=100&w=160',
+    'Mona': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/mona/image.png?strip=all&quality=100&w=160',
+    'Mualani': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/mualani/image.png?strip=all&quality=100&w=160',
+    'Ningguang': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/ningguang/image.png?strip=all&quality=100&w=160',
+    'Noelle': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/noelle/image.png?strip=all&quality=100&w=160',
+    'Qiqi': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/qiqi/image.png?strip=all&quality=100&w=160',
+    'Raiden': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/raiden_shogun/image.png?strip=all&quality=100&w=160',
+    'Razor': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/razor/image.png?strip=all&quality=100&w=160',
+    'Rosaria': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/rosaria/image.png?strip=all&quality=100&w=160',
+    'Sangonomiya Kokomi': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/sangonomiya_kokomi/image.png?strip=all&quality=100&w=160',
+    'Shenhe': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/shenhe/image.png?strip=all&quality=100&w=160',
+    'Sigewinne': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/sigewinne/image.png?strip=all&quality=100&w=160',
+    'Sucrose': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/sucrose/image.png?strip=all&quality=100&w=160',
+    'Tartaglia': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/tartaglia/image.png?strip=all&quality=100&w=160',
+    'Thoma': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/thoma/image.png?strip=all&quality=100&w=160',
+    'Venti': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/venti/image.png?strip=all&quality=100&w=160',
+    'Wanderer': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/wanderer/image.png?strip=all&quality=100&w=160',
+    'Xiao': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/xiao/image.png?strip=all&quality=100&w=160',
+    'Xingqiu': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/xingqiu/image.png?strip=all&quality=100&w=160',
+    'Xinyan': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/xinyan/image.png?strip=all&quality=100&w=160',
+    'Yae Miko': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/yae_miko/image.png?strip=all&quality=100&w=160',
+    'Yaoyao': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/yaoyao/image.png?strip=all&quality=100&w=160',
+    'Yelan': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/yelan/image.png?strip=all&quality=100&w=160',
+    'Zhongli': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/zhongli/image.png?strip=all&quality=100&w=160',
+    'Anemo Traveler': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/traveler_anemo/image.png?strip=all&quality=100&w=160', 
+    'Geo Traveler': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/traveler_geo/image.png?strip=all&quality=100&w=160', 
+    'Electro Traveler': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/traveler_electro/image.png?strip=all&quality=100&w=160', 
+    'Dendro Traveler': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/traveler_dendro/image.png?strip=all&quality=100&w=160', 
+    'Hydro Traveler': 'https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/traveler_hydro/image.png?strip=all&quality=100&w=160'
+}
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    selected_personagens = {}
+    selected_characters = []
 
     if request.method == 'POST':
-        # Receber as seleções do formulário
-        dps = request.form.get('dps')
-        subdps1 = request.form.get('subdps1')
-        subdps2 = request.form.get('subdps2')
-        support = request.form.get('support')
+        character = request.form.get('character')
+        character2 = request.form.get('character1')
+        character3 = request.form.get('character2')
 
-        # Verificar se todos os campos foram preenchidos
-        if not all([dps, subdps1, subdps2, support]):
-            flash('Por favor, preencha todos os campos para montar seu time.')
-            return redirect(url_for('index'))
+        returned_characters = [character, character2, character3]
 
-        # Garantir que os personagens selecionados sejam únicos
-        selected_personagens = {
-            'DPS': dps,
-            'Sub DPS 1': subdps1,
-            'Sub DPS 2': subdps2,
-            'Support': support
-        }
-        if len(set(selected_personagens.values())) < 4:
-            flash('Cada personagem no time deve ser único. Escolha diferentes personagens.')
-            return redirect(url_for('index'))
+        for character in returned_characters:
+            if character != '' and character not in selected_characters:
+                selected_characters.append(character)
+    
+    return render_template('index.html', characters=characters, selected_characters=selected_characters)
 
-    # Gerando as URLs para as imagens dos personagens
-    personagens_imagens = {personagem: gerar_url_imagem(personagem) for personagem in personagens}
+@app.route('/get_teams', methods=['GET'])
+def get_teams():
+    teams = [['Lisa', 'Jean', 'Mona', 'Diluc'], ['Yae Miko', 'Zhongli', 'Yelan', 'Yaoyao'], ['Thoma', 'Venti', 'Xiao', 'Yelan']]
+    return jsonify(teams)
 
-    return render_template('index.html', personagens=personagens, selected_personagens=selected_personagens, personagens_imagens=personagens_imagens)
+@app.route('/get_characters', methods=['GET'])
+def get_characters():
+    return jsonify(characters)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,96 @@
+document.querySelectorAll('.custom-select .option').forEach(function(option) {
+    option.addEventListener('click', function() {
+        let select = option.closest('.custom-select');
+        let selectedValue = option.getAttribute('data-value');
+        let currentSelectedOption = select.querySelector('.option[selected]');
+
+        if (currentSelectedOption === option) {
+            currentSelectedOption.removeAttribute('selected');
+            select.querySelector('.option:first-child').textContent = `Selecione seu ${select.id}`; 
+            select.querySelector('.option:first-child').removeAttribute('data-value');
+            select.querySelector('.option:first-child').classList.remove('selected');
+        } else {
+            if (currentSelectedOption) {
+                currentSelectedOption.removeAttribute('selected');
+                currentSelectedOption.classList.remove('selected');
+            }
+            option.setAttribute('selected', 'true');
+            option.classList.add('selected');
+            select.querySelector('.option:first-child').textContent = option.textContent;
+            select.querySelector('.option:first-child').setAttribute('data-value', selectedValue);
+        }
+    });
+});
+
+document.getElementById('team-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let selectedTeam = [];
+    document.querySelectorAll('.custom-select').forEach(function(select) {
+        let selectedOption = select.querySelector('.option[selected]');
+        if (selectedOption.textContent !== null && selectedOption.textContent !== '') {
+            selectedTeam.push(selectedOption.textContent.trim());
+        }
+    });
+    if (Object.keys(selectedTeam).length > 0) {
+        console.log('Time Selecionado:', selectedTeam);
+    }
+});
+
+document.getElementById('submit-button').addEventListener('click', async function() {
+    const teamsResponse = await fetch('/get_teams');
+    const teams = await teamsResponse.json();
+
+    const charactersResponse = await fetch('/get_characters');
+    const characters = await charactersResponse.json();
+
+    const teamDisplay = document.getElementById('team-display');
+    const recommendedTitle = document.getElementById('recommended-teams-title');
+    
+    teamDisplay.innerHTML = '';
+    recommendedTitle.classList.add('show');
+
+    teams.forEach((team, index) => {
+        const teamDiv = document.createElement('div');
+        teamDiv.classList.add('team');
+
+        const teamTitle = document.createElement('div');
+        teamTitle.classList.add('team-title');
+        teamTitle.textContent = `Time ${index + 1}`;
+        teamDiv.appendChild(teamTitle);
+
+        const teamCharactersDiv = document.createElement('div');
+        teamCharactersDiv.classList.add('team-characters');
+
+        team.forEach((characterName, idx) => {
+            const characterDiv = document.createElement('div');
+            characterDiv.classList.add('character');
+
+            const img = document.createElement('img');
+            img.src = characters[characterName]; 
+            img.alt = characterName;
+
+            const name = document.createElement('div');
+            name.classList.add('character-name');
+            name.textContent = characterName;
+
+            characterDiv.appendChild(img);
+            characterDiv.appendChild(name);
+            
+            teamCharactersDiv.appendChild(characterDiv);
+        });
+
+        teamDiv.appendChild(teamCharactersDiv);
+        teamDisplay.appendChild(teamDiv);
+    });
+
+    // Mostrar os times com animação
+    setTimeout(() => {
+        teamDisplay.classList.add('show');
+    }, 100);
+   
+    setTimeout(function() {
+        recommendedTitle.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }, 100);
+});
